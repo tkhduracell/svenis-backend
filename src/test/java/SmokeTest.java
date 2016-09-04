ce0da446cf5d1d10aecd9e7ba356581d0a52d58e
Filip Lindqvist <buggfille@gmail.com>
2016-09-04 21:37:29 +0200

    Moved db

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -1,18 +1,17 @@
 
-import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-import com.svenis.model.svenis.tables.Sessions;
-import com.svenis.model.svenis.tables.records.SessionsRecord;
+import com.svenis.db.model.svenis.tables.Sessions;
+import com.svenis.db.model.svenis.tables.records.SessionsRecord;
 import org.jooq.impl.DSL;
 import org.junit.Test;
 
 public class SmokeTest {
 
-  public static final String H2_DATABASE = "jdbc:h2:./target/svenisdb";
+  private static final String H2_DATABASE = "jdbc:h2:./db/svenis.data.db";
 
   @Test
   public void testQueryingAfterMigration() throws Exception {
@@ -21,8 +20,6 @@ public class SmokeTest {
           DSL.using(c)
               .selectFrom(Sessions.SESSIONS)
               .fetchArray();
-          ;
-
       assertEquals(1, result.length);
       assertEquals("my-app", result[0].getValue(Sessions.SESSIONS.APP_NAME));
     }
